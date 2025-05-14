7c3ee4b4a32f1af32a9548254b66f6d00c85b4a0
Dirk Wenzel <wenzel@cps-it.de>
2025-05-14 18:00:15 +0200

    initial setup

---
+++ rector.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
+use Rector\PostRector\Rector\NameImportingPostRector;
+use Rector\ValueObject\PhpVersion;
+use Ssch\TYPO3Rector\CodeQuality\General\ConvertImplicitVariablesToExplicitGlobalsRector;
+use Ssch\TYPO3Rector\CodeQuality\General\ExtEmConfRector;
+use Ssch\TYPO3Rector\Configuration\Typo3Option;
+use Ssch\TYPO3Rector\Set\Typo3LevelSetList;
+
+return RectorConfig::configure()
+    ->withConfiguredRule(ExtEmConfRector::class, [
+        ExtEmConfRector::ADDITIONAL_VALUES_TO_BE_REMOVED => [],
+    ])
+    ->withPaths([
+        __DIR__ . '/vendor/cpsit/t3import_export',
+        __DIR__ . '/vendor/dwenzel/t3extension-tools',
+        __DIR__ . '/packages/*',
+    ])
+    ->withPhpSets(php84: true)
+    ->withSets([
+        Typo3LevelSetList::UP_TO_TYPO3_13,
+    ])
+    ->withPHPStanConfigs([
+        Typo3Option::PHPSTAN_FOR_RECTOR_PATH,
+    ])
+    ->withPhpVersion(PhpVersion::PHP_84)
+    ->withImportNames(importShortClasses: false)
+    ->withRules([
+        ConvertImplicitVariablesToExplicitGlobalsRector::class,
+    ])
+    ->withSkip([
+        // @see https://github.com/sabbelasichon/typo3-rector/issues/2536
+        __DIR__ . '/**/Configuration/ExtensionBuilder/*',
+
+        NameImportingPostRector::class => [
+            'ext_localconf.php',
+            'ext_tables.php',
+            'ClassAliasMap.php',
+            __DIR__ . '/**/Configuration/*.php',
+            __DIR__ . '/**/Configuration/**/*.php',
+        ],
+    ])
+    ->withoutParallel()
+;
